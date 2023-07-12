d7bdec4b5cb7a26b334185b5cc083f0aeb13781b
Adrian_Bueno <adrian.s.bueno1@gmail.com>
2023-07-12 18:03:50 -0300

    remove new front

--- levels-rank-api-rust/src/main.rs
+++ levels-rank-api-rust/src/main.rs
@@ -8,8 +8,7 @@ pub mod models;
 
 use controllers::players_controller;
 use dotenv::dotenv;
-use rocket::{http::Method, http::Status, Request};
-use rocket_cors::{AllowedOrigins, CorsOptions};
+use rocket::{http::Status, Request};
 use serde_json::{json, Value};
 use std::sync::Arc;
 
@@ -29,15 +28,6 @@ fn default(status: Status, req: &Request) -> Value {
 async fn rocket() -> _ {
     dotenv().ok();
 
-    let cors = CorsOptions {
-        allowed_origins: AllowedOrigins::all(),
-        // Other CORS options like allowed_methods, allowed_headers, etc.
-        // can be configured here.
-        ..Default::default()
-    }
-    .to_cors()
-    .expect("Failed to create CORS middleware.");
-
     // prisma client
     let db = Arc::new(
         db::new_client()
@@ -49,7 +39,6 @@ async fn rocket() -> _ {
     db._db_push(false).await.unwrap();
 
     rocket::build()
-        .attach(cors)
         .manage(Context { db })
         .register("/", catchers![default])
         .mount("/players", players_controller::routes())
--- levels-rank-new-front/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Routes from "./routes";
-
-const App: React.FC = () => {
-  return <Routes />;
-};
-
-export default App;
