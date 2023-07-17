436a0c9ccc86916844df2c7c9ee0ab7061b042e5
Adrian_Bueno <adrian.s.bueno1@gmail.com>
2023-07-17 02:26:59 -0300

    separar cors

--- levels-rank-api-rust/src/main.rs
+++ levels-rank-api-rust/src/main.rs
@@ -3,15 +3,14 @@ extern crate rocket;
 extern crate rocket_cors;
 
 mod controllers;
+pub mod cors;
 pub mod db;
 pub mod helpers;
 pub mod models;
 
 use controllers::players_controller;
 use dotenv::dotenv;
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
-use rocket::{http::Status, Request, Response};
+use rocket::{http::Status, Request};
 
 use serde_json::{json, Value};
 use std::sync::Arc;
@@ -28,28 +27,6 @@ fn default(status: Status, req: &Request) -> Value {
     json!({ "status": status.code, "url": req.uri(), })
 }
 
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to responses",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new(
-            "Access-Control-Allow-Methods",
-            "POST, GET, PATCH, OPTIONS",
-        ));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
-}
-
 #[launch]
 async fn rocket() -> _ {
     dotenv().ok();
@@ -64,7 +41,7 @@ async fn rocket() -> _ {
     db._db_push(false).await.unwrap();
 
     rocket::build()
-        .attach(CORS)
+        .attach(cors::CORS)
         .manage(Context { db })
         .register("/", catchers![default])
         .mount("/players", players_controller::routes())
